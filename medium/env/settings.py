5c8c5d49573dc92534a91f6d5f10db04a17aa4e3
Chris Mcdonald <xwraithanx@gmail.com>
2010-11-14 05:36:58 -0800

    settings changes

--- medium/env/settings.py
+++ medium/env/settings.py
@@ -4,13 +4,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'. 
         'NAME': 'medium',                      # Or path to database file if using sqlite3.
-        'USER': 'medium-dev',                      # Not used with sqlite3.
-        'PASSWORD': '1234',                  # Not used with sqlite3.
+        'USER': 'medium',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
-
-
-    
+from local_settings import *
